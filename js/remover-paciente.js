c7a4611dbf39b3b66fe457b47c5d20cf678abb65
Lucas Miranda <lucascruz772@gmail.com>
2022-04-05 13:01:58 -0300

    V2. - Adicionar animação para a remoção.

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,13 @@
-var tabela = document.querySelector("table");
+var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(exclui){
  /* var alvoEvento = exclui.target;
   var paiAlvo = alvoEvento.parentNode;  --metodo com variavel
   paiAlvo.remove();*/
-  exclui.target.parentNode.remove();
+  exclui.target.parentNode.classList.add("esmaecer");
+
+    setTimeout(function(){
+        exclui.target.parentNode.remove();
+    }, 500);
+  
 });
